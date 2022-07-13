0017151557be8b410b0fdd065bc227dd001d42a3
BlackPrince163 <4523120100Niyaz>
2022-07-13 16:23:25 +0300

    + logout

--- ganarcrm_vue/src/views/dashboard/MyAccount.vue
+++ ganarcrm_vue/src/views/dashboard/MyAccount.vue
@@ -3,14 +3,37 @@
     <div className="columns is-multiline">
       <div className="column is-12">
         <h1 className="title">My account</h1>
+
+        <div class="column is-12">
+          <button @click="logout()" class="button is-danger">Log out</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "MyAccount"
+  name: "MyAccount",
+  methods: {
+    async logout() {
+      await axios
+          .post('/api/v1/token/logout')
+          .then(response => {
+            console.log('Logged out')
+          })
+          .catch(error => {
+            console.log(JSON.stringify(error))
+          })
+      axios.defaults.headers.common['Authorization'] = ''
+      localStorage.removeItem('token')
+      this.$store.commit('removeToken')
+
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
